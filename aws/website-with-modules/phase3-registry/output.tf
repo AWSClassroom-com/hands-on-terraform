ba1e659a496942fea8a05920c0248ce0da6d5e31
JohnHales <johnh@halestechnologies.com>
2026-03-09 18:05:59 +0000

    Fixed bucket in Phase 3

--- aws/website-with-modules/phase3-registry/output.tf
+++ aws/website-with-modules/phase3-registry/output.tf
@@ -1,3 +1,8 @@
+output "bucket_name" {
+  description = "The name of the S3 bucket to use for remote state"
+  value       = module.s3_bucket.bucket_name
+}
+
 output "load_balancer_dns" {
   value = module.load_balancer.dns_name
 }
